5cc70e0bd0bf2c41d30dba3019b9b311ee7b2964
jose antonio <ellocodelgorro@gmail.com>
2013-02-05 20:21:50 +0100

    intento de arreglo para que funcione el pull

--- js/tpl/principal/banner.js
+++ js/tpl/principal/banner.js
@@ -1,4 +1,5 @@
-<header id="banner" class="body">
+
+    <header id="banner" class="body">
 		<h1><a href="#">Smashing HTML5! <strong>HTML5 in the year <del>2022</del> <ins>2009</ins></strong></a></h1>
 		<nav><ul>
 			<li class="active"><a href="#">home</a></li>
