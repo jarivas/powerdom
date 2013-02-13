8ac62483d1369afa2e11bf4b5b677788537cefcf
jose antonio <ellocodelgorro@gmail.com>
2013-02-13 07:59:01 +0100

    Mas portfolio

---
+++ js/tpl/portfolio/banner.js
@@ -0,0 +1,35 @@
+function banner(){
+    this.html = '<header id="banner" class="body">'+
+        '<h1><a href="#">%%headerTitle%%</a></h1>'+
+        '%%nav_menu%%'+
+	'</header><!-- /#banner -->';
+
+    this.nav_menu = function(){
+    	var result = null;
+
+    	var list_items = [
+    		{
+    			"attributes" : {"class" : "active"},
+    			"text"  : Html.a({"href":"#"},"Home")
+    		},
+    		{
+    			"text"  : Html.a({"href":"#", "onclick" : "PD.go('portfolio');"},"portfolio")
+    		},
+    		{
+    			"text"  : Html.a({"href":"#"},"blog")
+    		},
+    		{
+    			"text"  : Html.a({"href":"#"},"contact")
+    		},
+    	];
+
+    	result = Html.nav(undefined, Html.ul(undefined, list_items));
+
+    	return result;
+    };
+
+}
+
+banner.prototype = new Template();
+
+Banner = new banner();
---
+++ js/tpl/portfolio/featured.js
@@ -0,0 +1,18 @@
+function featured(){
+    this.html =
+    '<aside id="featured" class="body"><article>'+
+        '<figure>'+
+        	'<img src="images/sm-logo.gif" alt="Smashing Magazine" />'+
+		'</figure>'+
+		'<hgroup>'+
+		'<h2>%%cabecera%%</h2>'+
+		'<h3><a href="#">%%subCabecera%%</a></h3>'+
+		'</hgroup>'+
+		'<p>%%contenido%%</p>'+
+	'</article></aside><!-- /#featured -->';
+
+}
+
+featured.prototype = new Template();
+
+Featured = new featured();
