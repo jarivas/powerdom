6fc6414d63bb09855c88cb0264b6b99fbb2bb178
Jose Rivas <joseantonio.rivasfernandez@gmail.com>
2018-10-10 17:47:16 +0200

    removing the / from paths in order to work with gihub pages

--- js/core/loadStaticComponents.js
+++ js/core/loadStaticComponents.js
@@ -1,7 +1,7 @@
-import DialogComponent from '/components/static/DialogComponent.js';
-import NotificationComponent from '/components/static/NotificationComponent.js';
-import ModalComponent from '/components/static/ModalComponent.js';
-import LoadingComponent from '/components/static/LoadingComponent.js';
+import DialogComponent from 'components/static/DialogComponent.js';
+import NotificationComponent from 'components/static/NotificationComponent.js';
+import ModalComponent from 'components/static/ModalComponent.js';
+import LoadingComponent from 'components/static/LoadingComponent.js';
 
 //Init
 DialogComponent.init();
