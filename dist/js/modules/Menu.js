055e01cfb65a1ce11576bc75734cf02330fc82a5
Jose Rivas <joseantonio.rivasfernandez@gmail.com>
2019-04-04 00:38:44 +0200

    menu working

--- dist/js/modules/Menu.js
+++ dist/js/modules/Menu.js
@@ -1,16 +1,38 @@
 class Menu {
+    static isBuilt() {
+        return Menu.prototype.hasOwnProperty('$li')
+    }
     static build() {
         const pages = PD.Config.get().pages
-        const $menu = PD.$('ul.menu')
+        const menu = PD.select('ul.menu')
+        const $menu = PD.$(menu)
 
         for (const [key, page] of Object.entries(pages)) {
             if(page.navigation) {
                 const html = `<li ${page.default ? 'class="active"' : ''}>
-                    <a href="#" data-page="${key}">${page.title}</a>
+                    <a href="#" data-index="${key}">${page.title}</a>
                 </li>`
                 $menu.prepend(html)
             }
         }
+
+        Menu.prototype.$li = PD.$('li', menu);
+
+        PD.$('a', menu).listen('click', Menu.changePage)
+    }
+
+    static changePage(e){
+        const el = e.target
+        const index = el.dataset.index
+
+        e.preventDefault()
+
+        PD.Pages.go(index)
+
+        Menu.prototype.$li.removeClass('active')
+
+        PD.$(el.parentNode).addClass('active')
+
     }
 }
 
